b7ed2bb540d031a366e6c2a5de79d9c4f01e3b8f
ldupas <louisdupas@net-c.com>
2020-06-11 11:45:24 +0200

    livecoding first entity book init

---
+++ src/Controller/BookController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Book;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BookController extends AbstractController
+{
+    /**
+     * @Route("/book", name="book")
+     */
+    public function index()
+    {
+        return $this->render('book/index.html.twig', [
+            'controller_name' => 'BookController',
+        ]);
+    }
+
+    /**
+     * @Route("/createbook", name="create_book")
+     */
+    public function createBook(): Response
+    {
+        // you can fetch the EntityManager via $this->getDoctrine()
+        // or you can add an argument to the action: createProduct(EntityManagerInterface $entityManager)
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $book = new Book();
+        $book->setName('The Grapes of Wrath');
+        $book->setAuthor('Steinbeck');
+        $book->setYear(1939);
+
+        // tell Doctrine you want to (eventually) save the Product (no queries yet)
+        $entityManager->persist($book);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+        return new Response('Saved new book with id '.$book->getId());
+    }
+}
